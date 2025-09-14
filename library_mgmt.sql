80a50688939cd74896591b2d8919536cc8f6b58f
Vijaykumarkurahatti <55583877+Vijaykumarkurahatti@users.noreply.github.com>
2025-09-14 18:24:54 +0530

    Update library_mgmt.sql

--- library_mgmt.sql
+++ library_mgmt.sql
@@ -9,3 +9,13 @@ create table Books(
   Published_year int,
   Genre varchar(50),
   Available_cpoies int)
+
+-- Members table
+create table Members(Member_id int primary key, Name varchar(100) not null, Email varchar(100) unique,Join_date Date)
+
+-- Borrwowed books
+create table Borrowed_books(
+  Borrwo_id int primary key,Member_id int, Borrow_date Date,
+  Retuen_date Date, foreign key (Book_id) references Books(Book_id),
+  foreign key (Member_id) referneces Members (Member_id) )
+
